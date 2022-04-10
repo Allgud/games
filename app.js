7d798d25d69345e5dc8692e5bbe93a0b8bd860b8
Allgud <allgud83@gmail.com>
2022-04-10 16:23:05 +0300

    third section without js

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const gameBtns = document.querySelectorAll('.play__button');
+const gameBtns = document.querySelectorAll('.play__button')
+const fruits = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин']
 
 const shuffleArray = arr => {
     const newArr = arr.slice()
     return newArr.sort(() => Math.random() - 0.5)
 }
-const fruits = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин']
 
 const getSeason = () => {
     const userNum = prompt('Введите число от 1 до 12')
     if(userNum > 12){
         alert("Нет столько месяцев в году, баклан!")
+        return
     }
+
     if(userNum < 1 || userNum === ''){
         alert('Пожалуйста, введите число от 1 до 12')
+        return
     }
+
     if(userNum == 12 || userNum == 1 || userNum == 2){
         alert('Зима')
+        return
     }
+
     if(userNum >= 3 && userNum <= 5){
         alert('Весна')
         return
     }
+
     if(userNum >= 6 && userNum <= 8){
         alert("Лето")
         return
     }
+
     if(userNum >= 9 && userNum <= 11){
         alert("Осень")
         return
@@ -40,21 +48,24 @@ const rememberWords = () => {
   answer1 = answer1.toLowerCase().trim()
   let answer2 = prompt(`Какое слово было последним в списке?`)
   answer2 = answer2.toLowerCase().trim()
+
   if(answer1 === firstWord && answer2 === lastWord){
       alert('Всё верно!')
       return
   }
+
   if(answer1 === firstWord && answer2 !== lastWord || answer1 !== firstWord && answer2 === lastWord){
       alert('Вы были близки')
       return
   }
+
   if(answer1 !== firstWord && answer2 !== lastWord){
       alert('Все ответы неверны')
       return
   }
 }
 
-gameBtns.forEach((btn,i) => {
+gameBtns.forEach(btn => {
     btn.addEventListener("click", e => {
         e.preventDefault()
         const parent = e.target.parentNode
